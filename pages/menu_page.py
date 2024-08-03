6585a5e9e6d27a2056381ee7cd7613907d79ca3e
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2024-08-03 03:34:31 +0300

    added coffee_cup_title, double_click_on_coffee_cup_title methods

--- pages/menu_page.py
+++ pages/menu_page.py
@@ -8,5 +8,13 @@ class MenuPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
+        self.coffee_cups = page.get_by_role("listitem").filter(has=page.locator(".cup"))
+
     def click_on_coffee_cup(self, coffee_type):
         self.page.get_by_label(coffee_type, exact=True).click()
+
+    def coffee_cup_title(self, coffee_type):
+        return self.coffee_cups.filter(has=self.page.get_by_label(coffee_type, exact=True)).get_by_role("heading")
+
+    def double_click_on_coffee_cup_title(self, coffee_type):
+        self.coffee_cup_title(coffee_type).dblclick()
