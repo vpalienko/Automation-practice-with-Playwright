2e5b9ab3991c9d005000621fbd16121a2367eb47
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2024-08-03 15:40:17 +0300

    added smoke marker for one coffee_list data set

--- data/data_for_tests.py
+++ data/data_for_tests.py
@@ -1,4 +1,6 @@
-coffee_list = [("Espresso", "$10.00"),
+from pytest import mark, param
+
+coffee_list = [param("Espresso", "$10.00", marks=mark.smoke),
                ("Espresso Macchiato", "$12.00"),
                ("Cappuccino", "$19.00"),
                ("Mocha", "$8.00"),
