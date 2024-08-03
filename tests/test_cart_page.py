9f8de200b7627e7d8b327ec84619cf7a4f1f03a8
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2024-08-03 23:42:14 +0300

    added navigation_to_menu_page_from_cart_page test

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -8,3 +8,10 @@ def test_cart_is_empty_by_default(cart_page):
     expect(cart_page.cart_navbar_item).to_have_text("cart (0)")
     expect(cart_page.cart_content_items).not_to_be_visible()
     expect(cart_page.empty_cart_info_message).to_be_visible()
+
+
+@mark.smoke
+def test_navigation_to_menu_page_from_cart_page(cart_page, menu_page):
+    cart_page.open()
+    cart_page.navigate_to_menu()
+    expect(menu_page.page).to_have_url(menu_page.url)
