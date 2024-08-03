b34d7bdc8ec9db35612f04b682a0cc90382b9af2
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2024-08-03 03:47:39 +0300

    added data_for_tests file with coffee_list, coffee_translations data

--- tests/test_menu_page.py
+++ tests/test_menu_page.py
@@ -1,25 +1,6 @@
 from playwright.sync_api import expect
 from pytest import mark
-
-coffee_list = [("Espresso", "$10.00"),
-               ("Espresso Macchiato", "$12.00"),
-               ("Cappuccino", "$19.00"),
-               ("Mocha", "$8.00"),
-               ("Flat White", "$18.00"),
-               ("Americano", "$7.00"),
-               ("Cafe Latte", "$16.00"),
-               ("Espresso Con Panna", "$14.00"),
-               ("Cafe Breve", "$15.00")]
-
-coffee_translations = [("Espresso", "特浓咖啡"),
-                       ("Espresso Macchiato", "浓缩玛奇朵"),
-                       ("Cappuccino", "卡布奇诺"),
-                       ("Mocha", "摩卡"),
-                       ("Flat White", "平白咖啡"),
-                       ("Americano", "美式咖啡"),
-                       ("Cafe Latte", "拿铁"),
-                       ("Espresso Con Panna", "浓缩康宝蓝"),
-                       ("Cafe Breve", "半拿铁")]
+from data.data_for_tests import coffee_list, coffee_translations
 
 
 @mark.smoke
