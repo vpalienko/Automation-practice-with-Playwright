278249b50aff4d054da9373f6cb40fbf35d9a931
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2024-08-06 00:26:57 +0300

    added add_to_cart_dialog fixture

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page
 from pytest import fixture
+from modals.add_to_cart_dialog import AddToCartDialog
 from pages.cart_page import CartPage
 from pages.menu_page import MenuPage
 
@@ -12,3 +13,8 @@ def menu_page(page: Page):
 @fixture
 def cart_page(page: Page):
     return CartPage(page)
+
+
+@fixture
+def add_to_cart_dialog(page: Page):
+    return AddToCartDialog(page)
