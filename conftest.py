5ec1711681b0525e63e86f8201e078986069bf69
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2024-08-12 20:00:26 +0300

    added add_one_coffee_to_cart fixture

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page
 from pytest import fixture
+from data.data_for_tests import coffee_names
 from modals.add_to_cart_dialog import AddToCartDialog
 from pages.cart_page import CartPage
 from pages.menu_page import MenuPage
@@ -18,3 +19,9 @@ def cart_page(page: Page):
 @fixture
 def add_to_cart_dialog(page: Page):
     return AddToCartDialog(page)
+
+
+@fixture
+def add_one_coffee_to_cart(menu_page):
+    menu_page.open()
+    menu_page.click_on_coffee_cup(coffee_names[0])
