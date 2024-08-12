1f5460c0b02b47b061128c61cb9aba83d053f738
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2024-08-12 20:04:46 +0300

    updated MenuPage class: - added attributes related to cart preview - added hover_over_total_price_button method - added remove_hover_from_total_price_button method - added add_one_more_coffee_via_cart_preview method - added remove_one_coffee_from_cart_preview method

--- pages/menu_page.py
+++ pages/menu_page.py
@@ -9,6 +9,10 @@ class MenuPage(BasePage):
         super().__init__(page)
 
         self.coffee_cups = page.get_by_role("listitem").filter(has=page.locator(".cup"))
+        self.cart_preview = page.locator(".pay-container .cart-preview")
+        self.cart_preview_content = page.get_by_role("list").filter(has=page.locator(".list-item"))
+        self.cart_preview_add_button = self.cart_preview.get_by_role("button", name="add one")
+        self.cart_preview_remove_button = self.cart_preview.get_by_role("button", name="remove one")
 
     def click_on_coffee_cup(self, coffee_type, **kwargs):
         self.page.get_by_label(coffee_type, exact=True).click(**kwargs)
@@ -18,3 +22,15 @@ class MenuPage(BasePage):
 
     def double_click_on_coffee_cup_title(self, coffee_type):
         self.coffee_cup_title(coffee_type).dblclick()
+
+    def hover_over_total_price_button(self):
+        self.total_price_button.hover()
+
+    def remove_hover_from_total_price_button(self):
+        self.coffee_cups.first.hover()
+
+    def add_one_more_coffee_via_cart_preview(self):
+        self.cart_preview_add_button.click()
+
+    def remove_one_coffee_from_cart_preview(self):
+        self.cart_preview_remove_button.click()
