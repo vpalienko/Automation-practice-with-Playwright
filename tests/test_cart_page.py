e805f0a193d139efdbf4f2e4c7e56cb83cc2a557
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2024-08-26 03:41:11 +0300

    added all_added_types_of_coffee_are_displayed_in_cart test

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import expect
 from pytest import mark
+from data.data_for_tests import coffee_names, number_of_available_coffee
 
 
 @mark.smoke
@@ -15,3 +16,11 @@ def test_navigation_to_menu_page_from_cart_page(cart_page, menu_page, page):
     cart_page.open()
     cart_page.navigate_to_menu()
     expect(page).to_have_url(menu_page.url)
+
+
+@mark.usefixtures("add_all_coffee_to_cart")
+def test_all_added_types_of_coffee_are_displayed_in_cart(menu_page, cart_page):
+    menu_page.navigate_to_cart()
+    expect(cart_page.cart_navbar_item).to_have_text(f"cart ({number_of_available_coffee})")
+    expect(cart_page.cart_content).to_have_count(number_of_available_coffee)
+    expect(cart_page.cart_content).to_contain_text(sorted(coffee_names))
