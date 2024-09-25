901dc4ce9a5b079f171cc20f21a1b30232285bd9
Valerii Palienko <68478951+vpalienko@users.noreply.github.com>
2024-09-25 03:14:39 +0300

    removed cart navbar item text assertion from cart_is_empty_by_default test

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -6,7 +6,6 @@ from data.data_for_tests import coffee_names, number_of_available_coffee
 @mark.smoke
 def test_cart_is_empty_by_default(cart_page):
     cart_page.open()
-    expect(cart_page.cart_navbar_item).to_have_text("cart (0)")
     expect(cart_page.cart_content).not_to_be_visible()
     expect(cart_page.empty_cart_info_message).to_be_visible()
 
